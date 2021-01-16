085ebd69eca8c38eec4e61f9e72997c9ca46203b
Dan Ring <dan.ring@me.com>
2021-01-16 19:40:05 +0900

    completed back end

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,23 +1,34 @@
 class DosesController < ApplicationController
-
+  before_action :find_cocktail, except: [:destroy]
   def new
-    @cocktail = Cocktail.find("cocktail_id")
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
     @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-    render :new
+      render :new
     end
   end
 
   def destroy
-    @cocktail =
-    @dose = dose.find(params[:id])
+    @dose = Dose.find(params[:id])
+    # @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.destroy
-    redirect to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 end
